f0ed285b05a76f85c1416878f47e50a684acf369
Shawn Hiew <yvn7103@autuni.ac.nz>
2022-09-29 15:47:46 +1300

    Update to cards
    Updated cards

--- playmate/src/App.js
+++ playmate/src/App.js
@@ -1,23 +1,45 @@
 import React from "react";
 import './App.css';
 import Header from './Header';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
       <h1>ezyVet Hackerthon 2022 PlayMate😻🐶</h1>
-
-      {/* Header*/}
       <Header />
+      <Router>
+      <Switch>
+      <Route path="/chat">
+          <h1>I am chatpage</h1>
+        </Route>
+        <Route path="/petSit">
+          <h1>I am Pet Sit page</h1>
+        </Route>
+        <Route path="/Vet">
+          <h1>I am Vet booking page</h1>
+        </Route>
+        <Route path="/">
+          <h1>I am homepage</h1>
+        </Route>
+      </Switch>
+    </Router>
+    
 
- {/*tinder cards*/}
-    {/*butons at the bottom*/}
-
-    {/*chat screen */}
-    {/*seperate chat screen*/}
+      
 
     </div>
   );
 }
+
+{/* Header*/}
+/*<Header />*/
+
+{/*tinder cards*/}
+   {/*butons at the bottom*/}
+
+   {/*chat screen */}
+   {/*seperate chat screen*/}
 
 export default App;
